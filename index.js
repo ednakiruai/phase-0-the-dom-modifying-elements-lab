5b9d8095cd3e5c61e793cc723ff6d8d5227c38e4
ednakiruai <ednakiruai@gmail.com>
2024-04-03 19:33:22 +0300

    trial

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const trial =document.querySelector('main#main');
+trial.remove()
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML ="Edna is the champion";
